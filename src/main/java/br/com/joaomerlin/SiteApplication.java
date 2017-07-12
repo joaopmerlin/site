7838bba4183a0925f58f2b97210e3b5c8b164259
João Paulo <joao.p.merlin@gmail.com>
2017-07-11 21:37:26 -0300

    site

--- src/main/java/br/com/joaomerlin/SiteApplication.java
+++ src/main/java/br/com/joaomerlin/SiteApplication.java
@@ -2,8 +2,10 @@ package br.com.joaomerlin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+//@EnableCaching
 public class SiteApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/joaomerlin/controller/SiteController.java
+++ src/main/java/br/com/joaomerlin/controller/SiteController.java
@@ -1,5 +1,6 @@
 package br.com.joaomerlin.controller;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,8 +14,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class SiteController {
 
     @GetMapping
-    public String home(){return "index";}
+    @Cacheable("index")
+    public String index(){return "index";}
 
     @GetMapping("experiencia")
-    public String experiencia(){return "resume";}
+    @Cacheable("experiencia")
+    public String experiencia(){return "experiencia";}
+
+    @GetMapping("competencias")
+    @Cacheable("competencias")
+    public String competencias(){return "competencias";}
+
+    @GetMapping("contato")
+    @Cacheable("contato")
+    public String contato(){return "contato";}
+
+    @GetMapping("feedback")
+    @Cacheable("feedback")
+    public String feedback(){return "feedback";}
+
+    @GetMapping("blog")
+    @Cacheable("blog")
+    public String blog(){return "blog";}
 }
