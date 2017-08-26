864faa01d0d31a834c3667fc5b6885e511bc8223
joaopmerlin <joao.p.merlin@gmail.com>
2017-08-25 22:33:43 -0300

    update

--- src/main/java/br/com/joaomerlin/SiteApplication.java
+++ src/main/java/br/com/joaomerlin/SiteApplication.java
@@ -2,6 +2,7 @@ package br.com.joaomerlin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -13,5 +14,10 @@ public class SiteApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SiteApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SiteApplication.class);
 	}
 }
