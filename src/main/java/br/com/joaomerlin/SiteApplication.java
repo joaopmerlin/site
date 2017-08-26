2efd55ea5bc0f957250fe812f1a9b54e7567fd71
joaopmerlin <joao.p.merlin@gmail.com>
2017-08-25 21:58:31 -0300

    update

--- src/main/java/br/com/joaomerlin/SiteApplication.java
+++ src/main/java/br/com/joaomerlin/SiteApplication.java
@@ -2,13 +2,14 @@ package br.com.joaomerlin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
 @EnableCaching
-public class SiteApplication {
+public class SiteApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SiteApplication.class, args);
